f81feae470edd4f97f683905f62efdc23aacbb99
NamJiwony778 <71044421+NamJiwony778@users.noreply.github.com>
2021-05-31 16:10:12 -0400

    shop page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import './pages/homepage/homepage.styles.scss';
+import { Route, Switch } from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
-    <div >
-      <HomePage/>
-    </div>
+    <Switch>
+      <Route exact path="/" component={HomePage}/>
+      <Route exact path="/shop" component={ShopPage}/>
+    </Switch>
   );
 }
 
