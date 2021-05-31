807dc09bc77a4bb43ea4362168df00cb67b710aa
NamJiwony778 <71044421+NamJiwony778@users.noreply.github.com>
2021-05-31 16:30:24 -0400

    header added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import HomePage from './pages/homepage/homepage.component';
 import './pages/homepage/homepage.styles.scss';
 import { Route, Switch } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage}/>
-      <Route exact path="/shop" component={ShopPage}/>
-    </Switch>
+    <div>
+      <Header/>yarn
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+        <Route exact path="/shop" component={ShopPage}/>
+      </Switch>
+    </div>
   );
 }
 
